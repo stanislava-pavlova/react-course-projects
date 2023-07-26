6d3bc20610aa50fb8ffccc03f03ac9d9e8e48061
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-07-26 19:57:35 +0300

    load movie details

--- 04-usepopcorn/src/App.js
+++ 04-usepopcorn/src/App.js
@@ -134,6 +134,7 @@ export default function App() {
             <MovieDetails
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
+              KEY={KEY}
             />
           ) : (
             <>
