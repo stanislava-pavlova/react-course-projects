78a358ce4e22d840cb08550b98efb5e576beeec5
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-07-27 12:44:56 +0300

    add keypress event listener

--- 04-usepopcorn/src/components/MovieDetails.js
+++ 04-usepopcorn/src/components/MovieDetails.js
@@ -48,6 +48,23 @@ export default function MovieDetails({
 
   useEffect(
     function () {
+      function callback(e) {
+        if (e.code === 'Escape') {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
+  useEffect(
+    function () {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
