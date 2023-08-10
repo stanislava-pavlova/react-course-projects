b258930e7c8e82bd923f699da65c29d7e2793bc8
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-08-10 13:49:09 +0300

    implementing the Countries List

--- 07-worldwise/src/App.jsx
+++ 07-worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
+import CountryList from './components/CountryList';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -47,7 +48,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
 
--- 07-worldwise/src/components/Sidebar.jsx
+++ 07-worldwise/src/components/Sidebar.jsx
@@ -3,14 +3,14 @@ import AppNav from './AppNav';
 import Logo from './Logo';
 import styles from './Sidebar.module.css';
 
-export default function Sidebar() {
+function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
       <Outlet />
-      
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
@@ -19,3 +19,5 @@ export default function Sidebar() {
     </div>
   );
 }
+
+export default Sidebar;
