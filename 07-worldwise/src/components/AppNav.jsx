7e49075c59b91aabc19c9bf4135cb62143c70cc7
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-08-10 15:39:47 +0300

    programmatic navigation

--- 07-worldwise/src/components/AppNav.jsx
+++ 07-worldwise/src/components/AppNav.jsx
@@ -6,7 +6,7 @@ function AppNav() {
     <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="cites">Cities</NavLink>
+          <NavLink to="cities">Cities</NavLink>
         </li>
         <li>
           <NavLink to="countries">Countries</NavLink>
