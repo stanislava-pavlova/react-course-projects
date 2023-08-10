6ce476ff8cbb75c5453a5e080cc3d6657b255ce1
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-08-10 15:20:11 +0300

    storing state in the URL

--- 07-worldwise/src/components/Map.jsx
+++ 07-worldwise/src/components/Map.jsx
@@ -1,11 +1,26 @@
-import styles from './Map.module.css'
+import { useSearchParams } from 'react-router-dom';
+import styles from './Map.module.css';
 
 function Map() {
-    return (
-        <div className={styles.mapContainer}>
-            Map
-        </div>
-    )
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat} {lng}
+      </h2>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
