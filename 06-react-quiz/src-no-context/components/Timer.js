dc62a6c007563686ea798fa64482e76fa9328ef4
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-08-16 13:02:47 +0300

    refactor to Context API

--- 06-react-quiz/src-no-context/components/Timer.js
+++ 06-react-quiz/src-no-context/components/Timer.js
@@ -1,12 +1,9 @@
 import { useEffect } from 'react';
-import { useQuiz } from '../contexts/QuizContext';
 
-function Timer() {
-  const { dispatch, secondsRemaining } = useQuiz();
-
+function Timer({ dispatch, secondsRemaining }) {
   const mins = Math.floor(secondsRemaining / 60);
   const seconds = secondsRemaining % 60;
-
+  
   useEffect(
     function () {
       const id = setInterval(function () {
