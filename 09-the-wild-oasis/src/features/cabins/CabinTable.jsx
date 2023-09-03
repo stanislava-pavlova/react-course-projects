eaf278b514008a0c6cc1c1898861e8f69d08d330
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-09-03 17:50:02 +0300

    sort cabins functionality

--- 09-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 09-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -19,6 +19,13 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -34,7 +41,8 @@ function CabinTable() {
 
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- 09-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
+++ 09-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from '../../ui/TableOperations';
 import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,17 @@ function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No discount' },
           { value: 'with-discount', label: 'With discount' },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-desc', label: 'Sort by name (Z-A)' },
+          { value: 'regularPrice-asc', label: 'Sort by price (Low to High)' },
+          { value: 'regularPrice-desc', label: 'Sort by price (High to Low)' },
+          { value: 'maxCapacity-asc', label: 'Sort by capacity (Low to High)' },
+          { value: 'maxCapacity-desc', label: 'Sort by capacity (High to Low)' },
         ]}
       />
     </TableOperations>
