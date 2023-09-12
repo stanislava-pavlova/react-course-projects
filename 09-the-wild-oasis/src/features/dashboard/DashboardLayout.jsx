cf55df0e8ab62169b12818cee367aabfed944306
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-09-12 22:50:28 +0300

    display stays for current day

--- 09-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 09-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -17,12 +18,7 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isLoading: isLoadingStays,
-    numDays,
-  } = useRecentStays();
+  const { confirmedStays, isLoading: isLoadingStays, numDays } = useRecentStays();
   const { cabins, isLoading: isLoadingCabins } = useCabins();
 
   if (isLoadingBookings || isLoadingStays || isLoadingCabins)
@@ -36,6 +32,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
