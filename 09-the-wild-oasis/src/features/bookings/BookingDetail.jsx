e063c6fe55a94008e69d0de9dcd5aece7a8b9042
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-09-13 14:39:21 +0300

    fix bugs

--- 09-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 09-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -18,6 +18,7 @@ import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
 import { deleteBooking } from '../../services/apiBookings';
 import { useDeleteBooking } from './useDeleteBooking';
+import Empty from '../../ui/Empty';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -34,6 +35,7 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
 
